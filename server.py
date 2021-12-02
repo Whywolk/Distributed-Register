d6ff8c4d8e470591f6ef861ff4725ba626ccfe40
w0resT <71945861+w0resT@users.noreply.github.com>
2021-12-02 20:45:05 +0300

    Minor changes. tl:dr Fixed bugs with server start

--- server.py
+++ server.py
@@ -4,3 +4,5 @@ from server_app import app
 if __name__ == '__main__':
     if sys.argv[1] == 'port':
         app.run(debug=True, host='localhost', port=sys.argv[2])
+    else:
+        app.run(debug=True, host='localhost', port=5000)
