52955bc798f5610f4896a9c4ad9183dd5eff5110
Whywolk <alexshir2001@gmail.com>
2021-12-03 20:24:39 +0300

    Added first version layout

--- client.py
+++ client.py
@@ -1,46 +1,101 @@
 import requests
-import sys
+import tkinter as tk
+from tkinter import ttk
 
-if __name__ == "__main__":
-    response = None
-    server_port = 5000
-    argc = len(sys.argv) - 1
+class HttpClient:
+    @staticmethod
+    def get_users(port):
+        response = requests.get("http://localhost:" + port + "/users")
+        return response
 
-    try:
-        # Requests func
-        if sys.argv[1] == 'port':
-            if argc >= 2:
-                server_port = sys.argv[2]
+    @staticmethod
+    def get_user(port, uid):
+        response = requests.get("http://localhost:" + port + "/user",
+                                json={'uid': uid})
+        return response
 
-            if argc >= 3 and sys.argv[3] == 'get':
-                # port 500x get all
-                if argc == 3:
-                    response = requests.get("http://localhost:" + server_port + "/users")
+    @staticmethod
+    def create(port, name, password):
+        response = requests.post("http://localhost:" + port + "/user",
+                                 json={'name': name, 'password': password})
+        return response
 
-                # port 500x get uid
-                elif argc == 4:
-                    response = requests.get("http://localhost:" + server_port + "/user",
-                                            json={'uid': sys.argv[4]})
-
-            # port 500x post username password
-            elif argc >= 5 and sys.argv[3] == 'post':
-                response = requests.post("http://localhost:" + server_port + "/user",
-                                         json={'name': sys.argv[4], 'password': sys.argv[5]})
-
-            # port 500x delete uid username
-            elif argc >= 6 and sys.argv[3] == 'delete':
-                response = requests.delete("http://localhost:" + server_port + "/user",
-                                           json={'uid': sys.argv[4],
-                                                 'name': sys.argv[5],
-                                                 'password': sys.argv[6]})
-            else:
-                print("[Error] Bad arguments bro. Using: port 500x get/post/delete ...")
-        else:
-            print("[Error] Bad arguments bro. Using: port 500x ...")
-    except Exception as error:
-        print("[Exception error] " + str(error))
+    @staticmethod
+    def delete(port, uid, name, password):
+        response = requests.delete("http://localhost:" + port + "/user",
+                                   json={'uid': uid,
+                                         'name': name,
+                                         'password': password})
+        return response
 
 
-    if response is not None:
-        print(response.status_code, response.headers)
-        print(response.text)
+class ClientApp:
+    def __init__(self, master=None):
+        # build ui
+        self.main = tk.Tk() if master is None else tk.Toplevel(master)
+        self.top = ttk.Frame(self.main)
+        self.connection = ttk.Labelframe(self.top)
+        self.host_label = ttk.Label(self.connection)
+        self.host_label.configure(text='Host')
+        self.host_label.pack(side='top')
+        self.host_entry = ttk.Entry(self.connection)
+        self.host_entry.pack(side='top')
+        self.port_label = ttk.Label(self.connection)
+        self.port_label.configure(text='Port')
+        self.port_label.pack(side='top')
+        self.port_entry = ttk.Entry(self.connection)
+        self.port_entry.pack(side='top')
+        self.connection.configure(labelanchor='nw', relief='flat', text='Connection')
+        self.connection.pack(side='top')
+        self.top.pack(side='top')
+        self.middle = ttk.Frame(self.main)
+        self.actions = ttk.Labelframe(self.middle)
+        self.button1 = ttk.Button(self.actions)
+        self.button1.configure(text='Get user')
+        self.button1.pack(side='left')
+        self.button2 = ttk.Button(self.actions)
+        self.button2.configure(text='Get users')
+        self.button2.pack(side='left')
+        self.button3 = ttk.Button(self.actions)
+        self.button3.configure(text='Create user')
+        self.button3.pack(side='left')
+        self.button4 = ttk.Button(self.actions)
+        self.button4.configure(text='Delete user')
+        self.button4.pack(side='left')
+        self.actions.configure(height='200', takefocus=False, text='Actions', width='200')
+        self.actions.pack(side='left')
+        self.middle.configure(relief='flat', takefocus=False)
+        self.middle.pack(side='top')
+        self.bottop = ttk.Frame(self.main)
+        self.forms = ttk.Frame(self.bottop)
+        self.forms.configure(height='200', width='200')
+        self.forms.pack(side='top')
+        self.output = ttk.Frame(self.bottop)
+        self.output.configure(height='200', width='200')
+        self.output.pack(side='top')
+        self.bottop.configure(height='200', width='200')
+        self.bottop.pack(side='top')
+        self.main.minsize(300, 200)
+
+        # Main widget
+        self.mainwindow = self.main
+        self.mainwindow.title("Distributed Register")
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def render_find_users(self):
+        pass
+
+    def render_find_user_form(self):
+        pass
+
+    def render_create_user_form(self):
+        pass
+
+    def render_delete_user_from(self):
+        pass
+
+if __name__ == '__main__':
+    app = ClientApp()
+    app.run()
