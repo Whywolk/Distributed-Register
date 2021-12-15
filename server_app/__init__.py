812b67f49e97e4d2d2399d8e716dc422f31b598b
Whywolk <alexshir2001@gmail.com>
2021-12-16 00:22:41 +0300

    Added mention of a license

--- server_app/__init__.py
+++ server_app/__init__.py
@@ -1,3 +1,20 @@
+#    This file is a part of Distributed-Register source code
+#    Copyright (C) 2021  Authors:   Alex Shirshov <https://github.com/Whywolk>
+#                                   w0rest <https://github.com/w0resT>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
